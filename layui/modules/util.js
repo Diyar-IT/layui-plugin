958b2e115c25bef2d693bad4b30b8c2b5305a06f
地亚尔-IT <136627746+Diyar-IT@users.noreply.github.com>
2023-08-30 06:33:44 +0800

    Update util.js

--- layui/modules/util.js
+++ layui/modules/util.js
@@ -406,6 +406,20 @@ layui.define('jquery', function(exports){
       _body.on(eventType, '*['+ attr +']', util.event.UTIL_EVENT_CALLBACK[attr]);
       
       return obj;
+    },
+    //货币格式
+    money_format: function (number, places, symbol, thousand, decimal, clear) {
+      places = !isNaN(places = Math.abs(places)) ? places : 2;
+      symbol = symbol !== undefined ? symbol : "￥";
+      thousand = thousand || ",";
+      decimal = decimal || ".";
+      clear = clear || false;
+      var negative = number < 0 ? "-" : "";
+      var i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "";
+      var j = i.length;
+      j = j > 3 ? j % 3 : 0;
+      //var j = i.length > 3 ? j % 3 : 0;
+      return symbol + negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
     }
   };
 
